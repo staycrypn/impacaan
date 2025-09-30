45497956b1f0e9c5d74d13528b7f5d3fb0e4fbf6
staycrypn <moneydivers@gmail.com>
2025-09-30 18:52:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,21 +6,25 @@ from io import BytesIO
 
 # Mnemonica stack
 MNEMONICA = [
-    "4C", "2H", "7D", "3C", "4H", "6D", "AS", "5H", "9S", "2S", "QH", "3D", "QC",
-    "8H", "6S", "5S", "9H", "KC", "2D", "JH", "3S", "8S", "6H", "10C", "5D", "KD",
-    "2C", "3H", "8D", "5C", "KS", "JD", "8C", "10S", "KH", "JC", "7S", "10H", "AD",
-    "4S", "7H", "4D", "AC", "9C", "JS", "QD", "7C", "QS", "10D", "6C", "AH", "9D"
+    "4C","2H","7D","3C","4H","6D","AS","5H","9S","2S","QH","3D","QC",
+    "8H","6S","5S","9H","KC","2D","JH","3S","8S","6H","10C","5D","KD",
+    "2C","3H","8D","5C","KS","JD","8C","10S","KH","JC","7S","10H","AD",
+    "4S","7H","4D","AC","9C","JS","QD","7C","QS","10D","6C","AH","9D"
 ]
 
 CARD_URL = "https://deckofcardsapi.com/static/img/{code}.png"
 
-# Cache images to speed up
-@st.cache_data
-def load_card_image(code):
-    url = CARD_URL.format(code=code)
-    resp = requests.get(url)
-    img = Image.open(BytesIO(resp.content))
-    return img
+# Preload all card images once
+@st.cache_data(show_spinner=False)
+def preload_all_cards():
+    cache = {}
+    for code in MNEMONICA:
+        resp = requests.get(CARD_URL.format(code=code))
+        img = Image.open(BytesIO(resp.content))
+        cache[code] = img
+    return cache
+
+card_images = preload_all_cards()
 
 st.set_page_config(page_title="ACAAN Helper", layout="centered")
 st.title("ACAAN Helper")
@@ -34,11 +38,10 @@ if 'current_number' not in st.session_state:
 # Display
 col1, col2 = st.columns([2,1])
 with col1:
-    st.image(load_card_image(st.session_state.current_card), width=180)
+    st.image(card_images[st.session_state.current_card], width=180)
 with col2:
     st.markdown(f"### Position: {st.session_state.current_number}")
     if MNEMONICA.index(st.session_state.current_card)+1 == st.session_state.current_number:
-        # Tiny white dot
         st.markdown("<div style='width:6px;height:6px;background:white;border-radius:50%;'></div>", unsafe_allow_html=True)
 
 # Buttons
